66231a2254fbf343e3eb8c9d943573c4d6909281
lojaluckbaby-coder <lojaluckbaby@gmail.com>
2026-08-30 11:55:12 -0300

    Update [key].js

--- functions/api/image/[key].js
+++ functions/api/image/[key].js
@@ -1,1 +1,23 @@
+export async function onRequestGet({params,env}){
+  if(!env.BUCKET){
+    return new Response('R2 não configurado.',{status:503});
+  }
 
+  const key=decodeURIComponent(params.key||'');
+  if(!key){
+    return new Response('Imagem não encontrada.',{status:404});
+  }
+
+  const object=await env.BUCKET.get(key);
+
+  if(!object){
+    return new Response('Imagem não encontrada.',{status:404});
+  }
+
+  const headers=new Headers();
+  object.writeHttpMetadata(headers);
+  headers.set('Cache-Control','public,max-age=31536000,immutable');
+  headers.set('Access-Control-Allow-Origin','*');
+
+  return new Response(object.body,{headers});
+}
